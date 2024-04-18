a7b0d8dd957a71efd27326db215465541ebcb6ea
Toyyib <toriloye2@rrc.ca>
2024-04-17 22:35:29 -0500

    added subcompoenent stories to table

---
+++ src/stories/HeroImage/HeroImage.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ImageGallery from './HeroImage'; // Adjust the import path based on your file structure
+
+// Define metadata for the stories
+const meta: Meta = {
+  title: 'Example/HeroImage',
+  component: ImageGallery,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+// Define the type for the story
+type ImageGalleryStory = StoryObj<typeof ImageGallery>;
+
+// Define individual stories for different variants of the ImageGallery component
+export const Default: ImageGalleryStory = {
+  args: {
+    images: [
+      { src: '/toymage.jpg', alt: 'Toyyib Oriloye' },
+      { src: 'url.jpg', alt: 'Second image description' },
+
+    ],
+  },
+};
+
+
---
+++ src/stories/HeroImage/HeroImage.types.ts
@@ -0,0 +1,10 @@
+// Define the type for individual image data specific to ImageGallery component
+export type ImageGalleryData = {
+  src: string; // Source URL of the image
+  alt?: string; // Optional alt text for the image
+};
+
+// Define the props for the ImageGallery component
+//export type ImageGalleryProps = {
+  //images: ImageGalleryData[]; // Array of images
+//};
